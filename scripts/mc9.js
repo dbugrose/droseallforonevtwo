eb09d222a0c00fc8a473b8c589f5ee303a160a76
dbugrose <darcy.ophelia.rose@gmail.com>
2026-02-11 12:19:45 -0800

    fixed magic eight ball display

--- scripts/mc9.js
+++ scripts/mc9.js
@@ -5,15 +5,24 @@ const AnswerParent = document.getElementById("answerParent");
 let data;
 
 async function fetchMC9() {
-    const response = await fetch(`https://allforone2526dor-fegsczb3g6hxf2dw.westus3-01.azurewebsites.net/api/mceight/RandomAnswer/${UserInput.value}%3F`)
+    const response = await fetch(`https://allforone2526dor-fegsczb3g6hxf2dw.westus3-01.azurewebsites.net/api/mceight/RandomAnswer`)
     data = await response.text();
     console.log(data);
 }
 
 async function Answer() {
+    if (!UserInput.value.includes("?")) {
+        {
+            AnswerParent.textContent = "Please enter a valid question.";
+            UserInput.value = "";
+        }
+    }
+    else {
         await fetchMC9();
         AnswerParent.textContent = data;
         UserInput.value = "";
+    }
+
 }
 
 EnterBtn.addEventListener("click", () => {
